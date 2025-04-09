0e75d17f3fe17fdba4afae997767db06ebe6666f
VinayGaddamGG <vinayrgaddam@gmail.com>
2025-04-08 19:13:44 -0500

    Update
    [ghstack-poisoned]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import HelloMessage from './components/HelloMessage';
+import Footer from './components/Footer';
 
 function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div className="p-4">
+      <Footer></Footer>
       <button onClick={() => setShow(!show)}>Toggle Hello</button>
       {show && <HelloMessage />}
     </div>
